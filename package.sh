0adc3e210da59d40986486cf9fba57fbbc5427d0
Ryan Emerson <remerson@redhat.com>
2015-11-13 12:57:27 +0000

    Streams file is now loaded remotely from aphrodite github repository.

--- package.sh
+++ package.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 grunt build --force
-wget -q https://raw.githubusercontent.com/jboss-set/aphrodite/master/src/main/resources/default.streams.json -O default.streams.json
-mv default.streams.json dist/
 git add dist/
